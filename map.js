90d43a7f39b77b6569ac8dea16704b91df995351
ajayvsingh <31656358+ajayvsingh@users.noreply.github.com>
2018-04-17 12:37:42 -0400

    updated a little

--- map.js
+++ map.js
@@ -5,20 +5,21 @@ function initMap() {
     center: {lat: 42.374, lng: -71.117},
     mapTypeId: 'roadmap'
   });
-
-  d3.csv("/harvardcrime/harvard_crime_incidents.csv", function(data) {
-  console.log(data[0]);
-  });
   
   function getMarkers() {
       let start = "<ul>";
-      for (i = 0; i < data.length; i++) {  
-        marker = new google.maps.Marker({
-             position: new google.maps.LatLng(data[i][9], data[i][10]),
-             map: map
-            )};
-        start += "<li><a href='" + data[i][0] + "' target='_blank'>" + "Incident type" + data[i][1] + "' target='_blank'>" + "Beginning of Occurrence" + data[i][2] + "' target='_blank'>"+ "Ending of Occurrence" + data[i][3] + "' target='_blank'>" + "Location" + data[i][4] + "' target='_blank'>" + "Address" + data[i][5] + "</a></li>
-   }
+      d3.csv("https://raw.githubusercontent.com/HarvardOpenData/harvardcrime/master/harvard_crime_incidents.csv", function(data) {
+          for (i = 0; i < data.length; i++) {
+            marker = new google.maps.Marker({
+                 position: new google.maps.LatLng(data[i][9], data[i][10]),
+                 map: map
+            });
+            start += "<li><a href='" + data[i][0] + "' target='_blank'>" + "Incident type" + data[i][1] + "' target='_blank'>" + "Beginning of Occurrence" + data[i][2] + "' target='_blank'>"+ "Ending of Occurrence" + data[i][3] + "' target='_blank'>" + "Location" + data[i][4] + "' target='_blank'>" + "Address" + data[i][5] + "</a></li>";
+          }
+      });
+  }
+}
+   
         
     heatmap = new google.maps.visualization.HeatmapLayer({
           data: getPoints(),

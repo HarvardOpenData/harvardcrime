d865f2e38fadade15b9d570d4fcbe9fe61b5d947
ajayvsingh <31656358+ajayvsingh@users.noreply.github.com>
2018-04-17 13:54:08 -0400

    updated!

--- map.js
+++ map.js
@@ -7,15 +7,17 @@ function initMap() {
   });
   
   function getMarkers() {
-      let start = "<ul>";
       d3.csv("https://raw.githubusercontent.com/HarvardOpenData/harvardcrime/master/harvard_crime_incidents.csv", function(data) {
           for (i = 0; i < data.length; i++) {
             marker = new google.maps.Marker({
                  position: new google.maps.LatLng(data[i][9], data[i][10]),
                  map: map
             });
-            start += "<li><a href='" + data[i][0] + "' target='_blank'>" + "Incident type" + data[i][1] + "' target='_blank'>" + "Beginning of Occurrence" + data[i][2] + "' target='_blank'>"+ "Ending of Occurrence" + data[i][3] + "' target='_blank'>" + "Location" + data[i][4] + "' target='_blank'>" + "Address" + data[i][5] + "</a></li>";
+            var infowindow = new google.maps.InfoWindow({
+                content: "<ul>" + "<li><a href='" + data[i][0] + "' target='_blank'>" + "Incident type" + data[i][1] + "' target='_blank'>" + "Beginning of Occurrence" + data[i][2] + "' target='_blank'>"+ "Ending of Occurrence" + data[i][3] + "' target='_blank'>" + "Location" + data[i][4] + "' target='_blank'>" + "Address" + data[i][5] + "</a></li>"
+            });
           }
       });
   }
 }
+   
